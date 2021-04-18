35b445e2be3e77d9f062bea2cee1a4e4da533b4f
Tim <tymekch@pm.me>
2021-04-18 13:20:39 +0200

    Jinja templating. Form class

--- spending_manager/static/render-login.js
+++ spending_manager/static/render-login.js
@@ -1,12 +1,10 @@
 class Account_Login extends React.Component {
   render() {
     return (
-        <div >
-            <input type="text" id="login" placeholder="Proszę podać login"></input>
-            <input type="password"  id="password" placeholder="Proszę podać hasło"></input>
-            <button type="button"  onclick="Login(); return false;">Zaloguj się</button>
+        <div style={appStyle}>
+            <LoginForm/>
         </div>
         );
   }
 }
-ReactDOM.render(<Account_Login/>, document.getElementById("login"));
+ReactDOM.render(<Account_Login/>, document.getElementById("root"));
--- spending_manager/static/render-register.js
+++ spending_manager/static/render-register.js
@@ -1,13 +1,11 @@
 class Account_Register extends React.Component {
   render() {
     return (
-        <div >
-            <input type="text" id="login" placeholder="Proszę podać login"></input>
-            <input type="password"  id="password" placeholder="Proszę podać hasło"></input>
-            <input type="password"  id="password-check" placeholder="Proszę ponownie podać hasło"></input>
-            <button type="button"  onclick="Register(); return false;">Zarejestruj się</button>
+        <div style={appStyle}>
+            <RegisterForm/>
         </div>
         );
   }
 }
-ReactDOM.render(<Account_Register/>, document.getElementById("register"));
+
+ReactDOM.render(<Account_Register/>, document.getElementById("root"));
