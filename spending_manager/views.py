9b28cc771ec5df99d835dff127e6b0ba516a4a36
Kamil Sudoł <61806373+kamilsudol@users.noreply.github.com>
2021-05-01 22:08:15 +0200

    Api usage templates added

--- spending_manager/views.py
+++ spending_manager/views.py
@@ -8,9 +8,11 @@ import json
 def hello_world():
     return render_template('index.html')
 
+
 @app.route('/login')
 def account_login():
     return render_template('account_login.html')
+
 
 @app.route('/register')
 def account_register():
@@ -30,3 +32,50 @@ def api_login():
 
     access_token = create_access_token(identity="deadbeef")
     return jsonify({"token": access_token})
+
+
+@app.route('/api/v1/registration', methods=['POST'])
+def api_registration():
+    result = json.loads(str(request.get_data()).split("\'")[1])
+    print(result)
+
+    username = result['username']
+    password = result['password']
+
+    # TODO wrzucic zapytanie do bazy, czy taki uzytkownik juz istnieje
+    # TODO jezeli jest ok, to wrzucic do bazy
+
+    ok = True
+
+    return jsonify({"success": ok})
+
+
+@app.route('/api/v1/transactions/create', methods=['POST'])
+def api_transactions_create():
+    # TODO
+    return jsonify({"success": True})
+
+
+@app.route('/api/v1/transactions/get', methods=['GET'])
+def api_transactions_get():
+    # TODO
+    return jsonify({"success": True})
+
+
+@app.route('/api/v1/transactions/update', methods=['POST'])  # moze lepsze bedzie tutaj PUT?
+def api_transactions_update():
+    # TODO
+    return jsonify({"success": True})
+
+
+@app.route('/api/v1/categories/get', methods=['GET'])
+def api_categories_get():
+    # TODO
+    return jsonify({"success": True})
+
+
+
+@app.route('/api/v1/categories/create', methods=['POST'])
+def api_categories_get():
+    # TODO
+    return jsonify({"success": True})
