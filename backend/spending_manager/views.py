30531319f1382c9d5d211ecfbc495cb6be258110
Gabriela Leśniak <gabcia100@gmail.com>
2021-05-29 23:50:07 +0200

    rewrite update balance functions on new version

--- backend/spending_manager/views.py
+++ backend/spending_manager/views.py
@@ -1,5 +1,5 @@
 from spending_manager import app, jwt
-from spending_manager.models import User, Transaction, Account
+from spending_manager.models import User, Transaction, Account, TransactionType
 
 from flask import render_template, jsonify, request, redirect, make_response
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, set_access_cookies, get_jwt
@@ -7,6 +7,8 @@ from datetime import datetime, timedelta, timezone, date
 
 import json
 import hashlib
+from bson.objectid import ObjectId
+
 
 def hash_password(password):
     s = hashlib.sha3_224()
@@ -26,8 +28,6 @@ def refresh_expiring_jwts(response):
         return response
     except (RuntimeError, KeyError):
         return response
-
-
 
 
 @jwt.token_in_blocklist_loader
@@ -61,6 +61,7 @@ def account_register():
 def database_route_test():
     return render_template('db_test.html')
 
+
 @app.route("/logout")
 @jwt_required(optional=True)
 def logout():
@@ -70,9 +71,10 @@ def logout():
 
 
 @app.route('/new_acc')
-#@jwt_required()
+# @jwt_required()
 def account_new_acc():
     return render_template('account_new_acc.html')
+
 
 @app.route('/api/v1/login', methods=['POST'])
 def api_login():
@@ -81,7 +83,7 @@ def api_login():
         p = request.json.get("password", None)
 
         user = User.objects(username=u).first()
-        #print(user.to_json())
+        # print(user.to_json())
 
         hashed_password = hash_password(p)
         if user.password == hashed_password:
@@ -107,7 +109,6 @@ def api_registration():
     return jsonify({"success": False}), 400
 
 
-
 @app.route('/api/v1/transactions/create', methods=['POST'])
 @jwt_required()
 def api_transactions_create():
@@ -117,22 +118,25 @@ def api_transactions_create():
         # TODO: Transaction(**request.json).save()
         Transaction(account_id=account_id,
                     amount=amount,
-                    category_id = request.json.get("category_id", None),
-                    transaction_type = request.json.get("transaction_type", None),
-                    other_account_id = request.json.get("other_account_id", None),
-                    transaction_status = request.json.get("transaction_status", None),
-                    person = request.json.get("person", None),
-                    recipient = request.json.get("recipient", None),
-                    transaction_date = str(date.today()),
-                    cyclic_period = request.json.get("cyclic_period", None)
+                    category_id=request.json.get("category_id", None),
+                    transaction_type=request.json.get("transaction_type", None),
+                    other_account_id=request.json.get("other_account_id", None),
+                    transaction_status=request.json.get("transaction_status", None),
+                    person=request.json.get("person", None),
+                    recipient=request.json.get("recipient", None),
+                    transaction_date=str(date.today()),
+                    cyclic_period=request.json.get("cyclic_period", None)
                     ).save()
-        acc = Account.objects(id=account_id).first()
-        acc.update(balance=(acc.balance+amount))
+        # acc = Account.objects(id=account_id).first()
+        # acc.update(balance=(acc.balance + amount))
+        transaction_type = request.json.get("transaction_type", None)
+        other_account_id = request.json.get("other_account_id", None)
+        update_balance_on_insert(account_id, transaction_type, amount, other_account_id)
         return jsonify({"success": True}), 200
     return jsonify({"success": False}), 400
 
 
-@app.route('/api/v1/transactions/get', methods=['POST','GET'])
+@app.route('/api/v1/transactions/get', methods=['POST', 'GET'])
 @jwt_required()
 def api_transactions_get():
     if request.is_json:
@@ -153,10 +157,10 @@ def api_transactions_update():
         value = request.json.get("value", None)
 
         tx = Transaction.objects(id=transaction_id).first()
+        before_update_data = get_transaction_data(tx)
         tx.update(attribute=attribute, value=value)
-
-        acc = Account.objects(id=tx.account_id).first()
-        acc.update(balance=(acc.balance+value))
+        after_update_data = get_transaction_data(tx)
+        update_balance_on_update(before_update_data, after_update_data)
         return jsonify({"success": True}), 200
 
     return jsonify({"success": True}), 400
@@ -168,7 +172,9 @@ def api_transactions_delete():
     if request.is_json:
         transaction_id = request.json.get("transaction_id", None)
         tx = Transaction.objects(id=transaction_id).first()
+        before_delete_data = get_transaction_data(tx)
         tx.delete()
+        update_balance_on_delete(before_delete_data)
     return jsonify({}), 200
 
 
@@ -206,3 +212,46 @@ def api_accounts_create():
             Account(user_id=user.id, name=name, balance=balance).save()
             return jsonify({"message": "Poprawnie dodano konto"}), 200
     return jsonify({"success": False, "mssg": "Brak danych"}), 400
+
+
+def update_balance_on_insert(account_id, transaction_type, amount, other_account_id):
+    account = Account.objects(id=account_id).first()
+    new_balance = account.balance
+    if transaction_type == TransactionType.EXPENSE or transaction_type == TransactionType.LENT:
+        new_balance -= amount
+    elif transaction_type == TransactionType.TRANSFER and ObjectId.is_valid(other_account_id):
+        new_balance -= amount
+        other_account = Account.objects(id=other_account_id).first()
+        new_other_balance = other_account.balance + amount
+        other_account = Account.objects(id=other_account_id).first()
+        other_account.update(balance=new_other_balance)
+    elif transaction_type == TransactionType.INCOME or transaction_type == TransactionType.BORROWED:
+        new_balance += amount
+
+    account.update(balance=new_balance)
+
+
+def get_transaction_data(transaction):
+    amount = transaction.amount
+    account_id = transaction.account_id
+    transaction_type = transaction.transaction_type
+    other_account_id = transaction.other_account_id
+    if transaction_type == TransactionType.TRANSFER:
+        other_account_id = transaction['other_account_id']
+    return {
+        "account_id": account_id,
+        "transaction_type": transaction_type,
+        "amount": amount,
+        "other_account_id": other_account_id
+    }
+
+
+def update_balance_on_update(before_update_data, after_update_data):
+    update_balance_on_delete(before_update_data)
+    update_balance_on_insert(after_update_data["account_id"], after_update_data["transaction_type"],
+                             after_update_data["amount"], after_update_data["other_account_id"])
+
+
+def update_balance_on_delete(before_delete_data):
+    update_balance_on_insert(before_delete_data["account_id"], before_delete_data["transaction_type"],
+                             -before_delete_data["amount"], before_delete_data["other_account_id"])
