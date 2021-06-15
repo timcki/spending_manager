2f84fa0223efe4c6fef80a6be304bdd711bf6a53
maciekczech <czechmaciej66@gmail.com>
2021-06-15 22:31:12 +0200

    username and user panel CSS tweaks

--- frontend/src/main_page/Avatar.js
+++ frontend/src/main_page/Avatar.js
@@ -1,21 +1,25 @@
-import React,{useContext} from 'react';
+import React, { useContext } from 'react';
 import avatar_img from '../images/logo.png';
 import '../styles/Avatar.css';
-import {AppContext} from "../store/AppContext";
+import { AppContext } from '../store/AppContext';
+import { PersonCircle } from 'react-bootstrap-icons';
 
-const Avatar = ()=>{
-    
-    const {user} = useContext(AppContext);
+const Avatar = () => {
+	const { user } = useContext(AppContext);
 
-    return(
-        <section className="avatar">
-            <div>
-                <img src={avatar_img} alt="logo użytkownika"/>
-            </div>
-            {user?<p>{user}</p>:null}
-        </section>
-    )
-}
-
+	return (
+		<section className="avatar">
+			<div>
+				<img src={avatar_img} alt="logo użytkownika" />
+			</div>
+			{user ? (
+				<div className="user-name-container">
+					{<PersonCircle className={'user-name-icon'} />}
+					<p className="user-name">{user}</p>
+				</div>
+			) : null}
+		</section>
+	);
+};
 
 export default Avatar;
